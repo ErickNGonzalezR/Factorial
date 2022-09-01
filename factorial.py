a752a841a86ed1bab55968981235b0c598253334
Erick Gonzalez <egonzalez48@uan.edu.co>
2022-09-01 12:00:40 -0500

    Factorial en python

--- factorial.py
+++ factorial.py
@@ -1,9 +1,6 @@
-int n = 0;
-        n = Integer.parseInt(JOptionPane.showInputDialog("Digite el numero al cual desea ver el factrial"));
-        int ac = 1;
-        while(n >= 1){
-            ac = ac*n;
-            n = n-1;
-        }
-        JOptionPane.showMessageDialog(null, ac);
-        
+ac = 1
+n = int(input("Digite el numero al cual desea ver el factrial"))
+while n >= 1:
+    ac = ac*n
+    n = n-1
+print(f"El factorial es: {ac}")
